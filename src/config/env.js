8eb52be7c1eac48e034f352d109d075d4a39a76b
m-faizan-adill <adilfaizan423@gmail.com>
2026-06-16 06:53:48 +0500

    feat(config): add JWT_SECRET and JWT_EXPIRES_IN env variables

--- src/config/env.js
+++ src/config/env.js
@@ -4,4 +4,6 @@ dotenv.config();
 
 export const PORT = process.env.PORT || 5000;
 export const MONGODB_URI = process.env.MONGODB_URI;
-export const FRONTEND_URL = process.env.FRONTEND_URL
+export const FRONTEND_URL = process.env.FRONTEND_URL;
+export const JWT_SECRET = process.env.JWT_SECRET;
+export const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN;
