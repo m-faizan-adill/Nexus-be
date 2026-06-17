63d622d865f00e78e995ca0f50057fba9f6d5480
m-faizan-adill <adilfaizan423@gmail.com>
2026-06-17 17:02:42 +0500

    feat: register auth and user routes in main router

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,12 @@
 import express from "express";
 
-import authRoutes from "./authRoutes.js";
+import authRoutes from "./auth.route.js";
+import userRoutes from "./users.route.js";
 
 const router = express.Router();
 
 router.use("/auth", authRoutes);
-// router.use("/users", userRoutes);
+router.use("/users", userRoutes);
 // router.use("/meetings", meetingRoutes);
 // router.use("/documents", documentRoutes);
 // router.use("/payments", paymentRoutes);
