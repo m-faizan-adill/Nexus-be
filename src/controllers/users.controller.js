70eb8b71ddd142da261978d2025f6459b7fd6a0c
m-faizan-adill <adilfaizan423@gmail.com>
2026-06-17 18:31:24 +0500

    refactor: delegate error handling to Express error middleware in users controller

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -4,17 +4,10 @@ import { updateUserProfile } from "../services/users.service.js";
 
 export const updateProfile = async (req, res) => {
   try {
-    const data = await updateUserProfile(
-      req.user._id,
-      req.body
-    );
+    const data = await updateUserProfile(req.user._id, req.body);
 
     successResponse(res, data, STATUS.OK);
   } catch (error) {
-    errorResponse(
-      res,
-      error.message,
-      error.statusCode || STATUS.SERVER_ERROR
-    );
+    next(err);
   }
 };
