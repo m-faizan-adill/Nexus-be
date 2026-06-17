ca7444596d6d7d3ab4671e9ead47c8339ea97d6f
m-faizan-adill <adilfaizan423@gmail.com>
2026-06-17 18:25:16 +0500

    refactor: use errorResponse helper and STATUS constant in role middleware

--- src/middlewares/role.middleware.js
+++ src/middlewares/role.middleware.js
@@ -1,10 +1,16 @@
+import { STATUS } from "../constants/statusCodes.js";
+import { errorResponse } from "../helpers/response.helper.js";
+
 export const authorizeRoles = (...roles) => {
-  return (req, res, next) => {
-    if (!roles.includes(req.user.role)) {
-      return res.status(403).json({
-        message: "Access denied: insufficient permissions",
-      });
-    }
-    next();
-  };
+    return (req, res, next) => {
+        if (!roles.includes(req.user.role)) {
+            return errorResponse(
+                res,
+                "Access denied: insufficient permissions",
+                STATUS.FORBIDDEN
+            );
+        }
+
+        next();
+    };
 };
