41ab69e82d2769fc38d97b0074133eacc5b737c4
m-faizan-adill <adilfaizan423@gmail.com>
2026-06-17 18:30:02 +0500

    refactor: use STATUS constants as default values in response helpers

--- src/helpers/response.helper.js
+++ src/helpers/response.helper.js
@@ -1,11 +1,13 @@
-export const successResponse = (res, data, statusCode = 200) => {
+import { STATUS } from "../constants/statusCodes.js";
+
+export const successResponse = (res, data, statusCode = STATUS.OK) => {
     return res.status(statusCode).json({
         success: true,
         ...data,
     });
 };
 
-export const errorResponse = (res, message, statusCode = 500) => {
+export const errorResponse = (res, message, statusCode = STATUS.SERVER_ERROR) => {
     return res.status(statusCode).json({
         success: false,
         message,
